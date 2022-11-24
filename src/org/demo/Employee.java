39d95dea045dcb75b58973159715f4ca5f7e7163
manojmanikam <manojkrishnasamy2002@gmail.com>
2022-11-24 11:17:24 +0530

    hi

---
+++ src/org/demo/Employee.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+public class Employee {
+
+	private void companyName() {
+		System.out.println("Itc");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Employee e = new Employee();
+		e.companyName();
+	}
+}
